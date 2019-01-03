3175f86fb3962370f3ac315c32dc81212ef51f7f
peterjohnhunt <git@peterjohnhunt.com>
2019-01-03 12:53:11 -0600

    WP CLI Explorer Items

--- src/classes/class-explorer.ts
+++ src/classes/class-explorer.ts
@@ -2,8 +2,8 @@ import { TreeView, ExtensionContext, TreeDataProvider, TreeItemCollapsibleState,
 import * as path from 'path';
 
 interface Item {
-    uri: Uri,
-    expandable: Boolean
+    uri: Uri;
+    expandable: Boolean;
 }
 
 export class Provider implements TreeDataProvider<Item> {
@@ -17,15 +17,80 @@ export class Provider implements TreeDataProvider<Item> {
         this._onDidChangeTreeData.fire();
     }
 
+    private details(section, object){
+        let items = [];
+        for (let key in object) {
+            let value = object[key];
+            if (value) {
+                let label = key.charAt(0).toUpperCase() + key.slice(1);
+                items.push({ uri: Uri.parse(path.join('wp://wpcli', section, `${label}: ${value}`)), expandable: false });
+            }
+        }
+        return items;
+    }
+
+    private items(section, array){
+        let items = [];
+        for (let index = 0; index < array.length; index++) {
+            const item = array[index];
+            items.push({ uri: Uri.parse(path.join('wp://wpcli', section, item) + '#' + index), expandable: true });            
+        }
+        return items;
+    }
+
     async getChildren(element?: Item): Promise<Item[]> {
+        const site = this.api.sites.getActive();
+
         if (element) {
+            if (element.uri.scheme == 'wp') {
+                let pieces = element.uri.path.split(path.sep);
+                let section = pieces[2];
+
+                switch (section) {
+                    case 'Info':
+                        return this.details(section, site.wpcli.getInfo());
+                    case 'Post Types':
+                        let posttypes = site.wpcli.getPostTypes();
+                        if (element.uri.fragment) {
+                            return this.details(section, posttypes[element.uri.fragment]);                            
+                        } else {
+                            return this.items(section, posttypes.map((type) => (type.name)));
+                        }
+                    case 'Taxonomies':
+                        let taxonomies = site.wpcli.getTaxonomies();
+                        if (element.uri.fragment) {
+                            return this.details(section, taxonomies[element.uri.fragment]);
+                        } else {
+                            return this.items(section, taxonomies.map((type) => (type.name)));
+                        }
+                    case 'Roles':
+                        let roles = site.wpcli.getRoles();
+                        if (element.uri.fragment) {
+                            return this.details(section, roles[element.uri.fragment]);
+                        } else {
+                            return this.items(section, roles.map((type) => (type.name)));
+                        }
+                }
+            }
+
             return [];
         }
 
-        const site = this.api.sites.getActive();
-
-        if (site) {
-            return [{ uri: Uri.parse(path.join('wp://wpcli', 'Version: ' + site.wpcli.getVersion())), expandable: false }];
+        if (site && site.wpcli) {
+            let items = [];
+            if (site.wpcli.hasInfo()) {
+                items.push({ uri: Uri.parse(path.join('wp://wpcli', 'Info', 'General')), expandable: true });
+            }
+            if (site.wpcli.hasPostTypes()) {
+                items.push({ uri: Uri.parse(path.join('wp://wpcli', 'Post Types')), expandable: true });                
+            }
+            if (site.wpcli.hasTaxonomies()) {
+                items.push({ uri: Uri.parse(path.join('wp://wpcli', 'Taxonomies')), expandable: true });
+            }
+            if (site.wpcli.hasRoles()) {
+                items.push({ uri: Uri.parse(path.join('wp://wpcli', 'Roles')), expandable: true });
+            }
+            return items;
         }
 
         return [];
@@ -47,7 +112,6 @@ export class Explorer {
         this.provider = treeDataProvider;
         this.treeView = window.createTreeView('wp-cli', { treeDataProvider });
     }
-
 
     refresh(){
         this.provider.refresh();
--- src/classes/class-wp-cli.ts
+++ src/classes/class-wp-cli.ts
@@ -1,12 +1,19 @@
 import { Disposable, EventEmitter, Event } from 'vscode';
+import { exec } from 'child_process';
+import async from 'async';
+
 import WP from 'wp-cli';
 import commandExists from 'command-exists';
+
+import * as path from 'path';
+import * as fs from 'fs';
 
 import { Config } from './class-config';
 
 export class WP_CLI{
     private _disposable: Disposable;
-    private _config: Config;
+    private config: Config;
+    private options: any;
 
     private wp;
 
@@ -14,15 +21,26 @@ export class WP_CLI{
     readonly onDidChangeData: Event<undefined> = this._onDidChangeData.event;
 
     version: string;
+    installed: boolean;
+    name: string;
+    url: string;
+    posttypes: Array<any>;
+    taxonomies: Array<any>;
+    roles: Array<any>;
 
     constructor(site){
-        this._config = new Config();
+        this.config = new Config();
+        this.options = { path: site.getRoot() };
 
+        this.initialize(this.options);
+    }
+
+    private initialize(options) {
         commandExists('wp', (err, exists) => {
             if (err) return;
 
-            if (exists){
-                WP.discover({ path: site.getRoot() }, (wp) => {
+            if (exists) {
+                WP.discover(options, (wp) => {
                     this.wp = wp;
 
                     this.setVersion();
@@ -31,18 +49,134 @@ export class WP_CLI{
         });
     }
 
-    private setVersion(){
-        this.wp.core.version((err,version) => {
-            if ( err ) return;
+    private setVersion() {
+        this.wp.core.version((err, version) => {
+            if (err && !version) return;
 
             this.version = version;
-            
+
+            this._onDidChangeData.fire();
+
+            if (version) {
+                this.setInstalled();
+            }
+        });
+    }
+
+    public getVersion() {
+        return this.version;
+    }
+
+    private setInstalled() {
+        this.wp.core.is_installed((err) => {
+            if (err) return;
+
+            this.installed = true;
+
+            this._onDidChangeData.fire();
+
+            this.setName();
+            this.setURL();
+            this.setPostTypes();
+            this.setTaxonomies();
+            this.setRoles();
+        });
+    }
+
+    public getInstalled() {
+        return this.installed;
+    }
+
+    public getName(){
+        return this.name;
+    }
+
+    private setName() {
+        this.wp.option.get('blogname', (err, name) => {
+            if (err && !name) return;
+
+            this.name = name;
+
             this._onDidChangeData.fire();
         });
     }
 
-    public getVersion(){
-        return this.version || 'loading';
+    public getURL() {
+        return this.url;
+    }
+
+    private setURL() {
+        this.wp.option.get('siteurl', (err, url) => {
+            if (err && !url) return;
+
+            this.url = url.replace(/(^\w+:|^)\/\//, '');
+
+            this._onDidChangeData.fire();
+        });
+    }
+
+    public hasPostTypes(){
+        return this.posttypes && this.posttypes.length;
+    }
+
+    public getPostTypes(){
+        return this.posttypes;
+    }
+
+    private setPostTypes(){
+        this.wp.post_type.list((err,posttypes) => {
+            if (err && !posttypes) return;
+
+            this.posttypes = posttypes;
+        });
+    }
+
+    public hasTaxonomies() {
+        return this.taxonomies && this.taxonomies.length;
+    }
+
+    public getTaxonomies() {
+        return this.taxonomies;
+    }
+
+    private setTaxonomies() {
+        this.wp.taxonomy.list((err, taxonomies) => {
+            if (err && !taxonomies) return;
+
+            this.taxonomies = taxonomies;
+        });
+    }
+
+    public hasRoles() {
+        return this.roles && this.roles.length;
+    }
+
+    public getRoles() {
+        return this.roles;
+    }
+
+    private setRoles() {
+        this.wp.role.list((err, roles) => {
+            if (err && !roles) return;
+
+            this.roles = roles;
+        });
+    }
+
+    public hasInfo(){
+        let info = this.getInfo();
+
+        for (let key in info) {
+            if (info[key]) return true;
+        }
+    }
+
+    public getInfo(){
+        return {
+            version: this.getVersion(),
+            name: this.getName(),
+            url: this.getURL()
+        };
     }
 
     public dispose() {
